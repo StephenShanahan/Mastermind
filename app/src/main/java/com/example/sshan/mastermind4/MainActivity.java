5271c89cd13e68bf218e376f71dcd66181dd9954
StephenShanahan <icecreamstephen@gmail.com>
2016-01-28 21:39:20 +0000

    Getting buttons to work for whole app

--- app/src/main/java/com/example/sshan/mastermind4/MainActivity.java
+++ app/src/main/java/com/example/sshan/mastermind4/MainActivity.java
@@ -11,14 +11,74 @@ import android.widget.Gallery;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{// implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       /* ImageButton btn1 = (ImageButton) findViewById(R.id.redPeg);
+        btn1.setOnClickListener(this);
+
+        ImageButton greenButton = (ImageButton) findViewById(R.id.greenPeg);
+        greenButton.setOnClickListener(this);*/
+
     }
+
+  /*  public void onClick(View v) {
+
+        Toast pieceToast=null;
+        ImageView imgBus;
+        String buttonID;
+        int x = 1;
+        int y = 1;
+
+      //  for(int x = 1;x<11;x++) {
+        //    for (int y = 1; y < 5; y++) {
+                switch (v.getId()) {
+
+                    case R.id.redPeg:
+                        //pieceToast= Toast.makeText(getApplicationContext(), "Image Button One Clicked", Toast.LENGTH_SHORT);
+                        //pieceToast.show();
+                        buttonID = "pegimage" + String.valueOf(x) + String.valueOf(y);
+                        int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                        imgBus = ((ImageButton) findViewById(resID));
+                        imgBus.setImageResource(R.drawable.red_peg_nobg);
+                        y++;
+                        break;
+
+                    case R.id.greenPeg:
+                        // pieceToast= Toast.makeText(getApplicationContext(), "Image Button Two Clicked", Toast.LENGTH_SHORT);
+                        //pieceToast.show();
+                        //adds source image to blank imageview on click
+                        //imgBus = (ImageView) findViewById(R.id.testimg3);
+                        imgBus = (ImageView) findViewById(R.id.pegimage23);
+                        imgBus.setImageResource(R.drawable.green_peg2_nobg);
+                        y++;
+                        break;
+
+         /*   case R.id.imageButton3:
+                pieceToast= Toast.makeText(getApplicationContext(), "Image Button Three Clicked", Toast.LENGTH_SHORT);
+                pieceToast.show();
+                break;
+
+            case R.id.imageButton4:
+                pieceToast= Toast.makeText(getApplicationContext(), "Image Button Four Clicked", Toast.LENGTH_SHORT);
+                pieceToast.show();
+                break;
+
+                    default:
+                        break;
+             //   }
+         //   }
+        }
+
+    }*/
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -42,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void select(View v)
+  /*  public void select(View v)
     {
 
         ImageView iv = new ImageView(this);
@@ -56,7 +116,27 @@ public class MainActivity extends AppCompatActivity {
         lp.addRule(RelativeLayout.ALIGN_LEFT);
         rl.addView(iv, lp);
 
-    }
+    }*/
+
+    public void select(View v)
+    {
+        ImageButton imgBug;
+        String buttonID;
+        int z = 21;
+        //String jumble = String.valueOf(z);
+
+        for(int x=0;x<2;x++) {
+            if (x == 0) {
+                buttonID = "pegimage" + String.valueOf(z);//"21"
+            } else {
+                buttonID = "testimg1";
+            }
+            int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+            imgBug = ((ImageButton) findViewById(resID));
+            imgBug.setImageResource(R.drawable.red_peg_nobg);
+        }
+
+    }//one for loop for row and one inner for loop for position. judge click off id pressed have all buttons flow into smae onclikc select method.
 
     public void selectBlue(View v)
     {
@@ -66,18 +146,6 @@ public class MainActivity extends AppCompatActivity {
         //imgBus = (ImageView) findViewById(R.id.testimg2);
         imgBus = (ImageView) findViewById(R.id.pegimage22);
         imgBus.setImageResource(R.drawable.blue_peg2_nobg);
-
-      /*  ImageView image = new ImageView(this);
-        image.setImageResource(R.drawable.blue_peg2);
-        RelativeLayout rl = (RelativeLayout) findViewById(R.id.gameRow1);
-        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT);
-        lp.addRule(RelativeLayout.BELOW, R.id.gameRow1);
-        //lp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-        lp.addRule(RelativeLayout.RIGHT_OF, image.getId());
-        rl.addView(iv, lp);*/
-
     }
 
     public void selectRed(View v)
@@ -90,7 +158,7 @@ public class MainActivity extends AppCompatActivity {
         //imgBus = (ImageView) findViewById(R.id.pegimage21);
         for( int x=0;x<2;x++) {
             if(x == 0) {
-                 buttonID = "pegimage21";
+                 buttonID = "pegimage" + "21";
             }
             else{
                 buttonID = "testimg1";
@@ -99,18 +167,8 @@ public class MainActivity extends AppCompatActivity {
             imgBus = ((ImageButton) findViewById(resID));
             imgBus.setImageResource(R.drawable.red_peg_nobg);
         }
-      /*  ImageView image = new ImageView(this);
-        image.setImageResource(R.drawable.blue_peg2);
-        RelativeLayout rl = (RelativeLayout) findViewById(R.id.gameRow1);
-        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT);
-        lp.addRule(RelativeLayout.BELOW, R.id.gameRow1);
-        //lp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-        lp.addRule(RelativeLayout.RIGHT_OF, image.getId());
-        rl.addView(iv, lp);*/
 
-    }
+    }// add a for loop y that relates to row position. have an undo method that removes the position from an array so it can go back into for loop?
 
     public void selectGreen(View v)
     {
@@ -120,17 +178,6 @@ public class MainActivity extends AppCompatActivity {
         //imgBus = (ImageView) findViewById(R.id.testimg3);
         imgBus = (ImageView) findViewById(R.id.pegimage23);
         imgBus.setImageResource(R.drawable.green_peg2_nobg);
-
-      /*  ImageView image = new ImageView(this);
-        image.setImageResource(R.drawable.blue_peg2);
-        RelativeLayout rl = (RelativeLayout) findViewById(R.id.gameRow1);
-        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT);
-        lp.addRule(RelativeLayout.BELOW, R.id.gameRow1);
-        //lp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-        lp.addRule(RelativeLayout.RIGHT_OF, image.getId());
-        rl.addView(iv, lp);*/
 
     }
 
@@ -143,16 +190,40 @@ public class MainActivity extends AppCompatActivity {
         imgBus = (ImageView) findViewById(R.id.pegimage24);
         imgBus.setImageResource(R.drawable.yellow_peg2_nobg);
 
-      /*  ImageView image = new ImageView(this);
-        image.setImageResource(R.drawable.blue_peg2);
-        RelativeLayout rl = (RelativeLayout) findViewById(R.id.gameRow1);
-        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT);
-        lp.addRule(RelativeLayout.BELOW, R.id.gameRow1);
-        //lp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-        lp.addRule(RelativeLayout.RIGHT_OF, image.getId());
-        rl.addView(iv, lp);*/
+    }
 
+    public int guessRow()
+    {
+        int x = 1;
+
+        while(x<11)
+        {
+           switch(x){
+               case 1:
+
+                   break;
+               case 2:
+           }
+
+
+            x++;
+        }
+
+        return x;
+    }
+
+    public int guessPosition()
+    {
+        int x = 1;
+        int y=0;
+
+        while(x<5)
+        {
+          y = x;
+
+        }
+
+        x++;
+        return y;
     }
 }
